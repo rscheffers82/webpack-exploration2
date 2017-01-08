45abfa63c7caf147a2c65f9f70c8aff149fc49ba
Roy Scheffers <r.scheffers@gmail.com>
2017-01-08 17:22:30 -0600

    Add GitHub build process automation

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,9 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       template: 'src/index.html'
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
     })
   ]
 };
