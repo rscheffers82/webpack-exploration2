254ffeedb9a12fec7edbfb1995debee8630c5fb0
Roy Scheffers <r.scheffers@gmail.com>
2017-01-08 12:11:52 -0600

    Split code dependencies into vendor.js and bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,30 @@
 var webpack = require('webpack');
 var path = require('path');
 
+const VENDOR_LIBS = [
+  'faker','lodash','react','react-dom','react-input-range',
+  'react-redux','react-router', 'redux', 'redux-form', 'redux-thunk'
+];
+
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    bundle: './src/index.js',
+    vendor: VENDOR_LIBS
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].js'
+    // [name].js: [name] will be replaced by the entry names
   },
   module: {
     rules: [
       {
         use: 'babel-loader',
-        test: /\.js$/
+        test: /\.js$/,
+        exclude: /node_modules/
       }, {
+        // css-loader loads all CSS
+        // style-loader places all CSS code into a style tag in bundle.js
         use: ['style-loader', 'css-loader'],
         test: /\.css$/
       }
