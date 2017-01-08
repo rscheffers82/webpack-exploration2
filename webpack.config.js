cb6a057659133bff8fe9d28f22ce638e5e9c6f0c
Roy Scheffers <r.scheffers@gmail.com>
2017-01-08 11:53:34 -0600

    enable babel and react

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,16 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/
+      }, {
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/
+      }
+    ]
   }
 };
